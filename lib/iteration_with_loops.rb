9efb26668c72e612bddb5e2e3bd64e784cc6ae63
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-04 18:24:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_even_values(src)
 row_index = 0 
 while row_index < src.length do 
   element_index = 0 
-  while element_index < src.length do 
+  while element_index < src[row_index].length do 
     if src[row_index][element_index].even?
     puts src[row_index][element_index]
   end 
