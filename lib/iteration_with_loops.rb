5d6ebea91415280d05c88dea3c021afbd0d89298
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-04 18:22:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ row_index = 0
 while row_index < src.length do 
   element_index = 0 
   while element_index < src.length do 
-    if src.even?
+    if src[row_index][element_index].even?
     puts src[row_index][element_index]
     element_index += 1
   end 
